4c91a1c27738b79c9ffb169e6280a9537b89a382
Pedro Resende <pedroluizrnunez@gmail.com>
2023-03-14 15:30:00 -0300

    requisito 2 completo

--- src/routes/talkerRoute.js
+++ src/routes/talkerRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { readAll } = require('../utils/readAndWriteFiles');
+const { readAll, readById } = require('../utils/readAndWriteFiles');
 
 const router = express.Router();
 
@@ -7,6 +7,18 @@ router.get('/', async (_req, res) => {
   const talker = await readAll();
   if (!talker) return res.status(200).json([]);
   return res.status(200).json(talker);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await readById(id);
+ 
+console.log(talker);
+  if (talker.length === 0) {
+    return res.status(404)
+    .json({ message: 'Pessoa palestrante não encontrada' }); 
+}
+  return res.status(200).json(talker[0]); 
 });
 
 module.exports = router;
--- src/utils/readAndWriteFiles.js
+++ src/utils/readAndWriteFiles.js
@@ -2,11 +2,17 @@ const fs = require('fs').promises;
 
 const readAll = async () => {
   const talkers = await fs.readFile('src/talker.json', 'utf-8');
-  console.log(talkers);
-  return JSON.parse(talkers);
+ return JSON.parse(talkers);
 };
 
+const readById = async (id) => {
+  const talkers = await readAll();
+  return talkers.filter((t) => t.id === Number(id));
+};
+
+readById();
 readAll();
 module.exports = {
   readAll,
+  readById,
 };
