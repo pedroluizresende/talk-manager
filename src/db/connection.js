0e8d22e38960d547bf4b8aba6d89133918a9dcf6
Pedro Resende <pedroluizrnunez@gmail.com>
2023-03-15 14:29:23 -0300

    fix requisito 12

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 // cria a conexão com o bando de dados connection
 const connection = mysql.createPool({
-  host: 'db',
-  user: 'root',
-  password: 'password',
-  port: '3306',
-  database: 'TalkerDB',
+  host: process.env.MYSQL_HOSTNAME,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  port: process.env.MYSQL_PORT,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = connection;
